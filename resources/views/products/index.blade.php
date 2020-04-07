34409c473a77b5333280533e5feb9713f5283bb7
Cojol0511 <phunghoangphuloc@gmail.com>
2020-04-07 17:25:40 +0700

    add commment git

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -2,9 +2,10 @@
 lay ten product
 {{$product->name}}
 
-lay ảnh
+
 
 
 @endforeach -->
+<!-- lay ảnh dau tien lafm anh dai dien, con cac anh sau cất trong dabáế -->
 <input type="hidden" name="" value="{{ $images = $product->images->get(0) }}">
 {{dd($images->image)}}
